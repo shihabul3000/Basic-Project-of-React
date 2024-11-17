5b327f4553769ba7214019b0b57fcb03fe5015bb
unknown <shihabul72@gmail.com>
2024-11-17 12:24:04 +0600

    first commit

---
+++ src/App.jsx
@@ -0,0 +1,14 @@
+import "./Components/App.css";
+import Hero from "./Components/Hero";
+import Navigation from "./Components/Navigation";
+
+const App = () => {
+    return (
+        <div>
+ <Navigation></Navigation>
+ <Hero></Hero>
+        </div>
+    );
+};
+
+export default App;
---
+++ src/Components/Hero.jsx
@@ -0,0 +1,52 @@
+
+
+const Hero = () => {
+    return (
+        <main className="hero container">
+<div className="hero-ncotent">
+
+<h1>YOUR FEET DESERVE THE BEST</h1>
+<p>YOUR FEET DESERVE THE BEST YOUR FEET DESERVE THE BESTYOUR FEET DESERVE THE BESTYOUR FEET DESERVE THE BEST</p>
+
+
+
+
+<div className="hero-button" >
+<button >Shop Now</button>
+
+<button className="secondary-button" >Category</button>
+</div>
+
+
+
+{/* Brands Icons  */}
+
+
+<div className="shopping">
+    <p>Aslo Available On</p>
+    <div className="brand-icons"><img src="/public/images/flipkart.png" alt="" />
+    
+    <img src="/public/images/amazon.png" alt="" />
+    
+    </div>
+
+</div>
+
+</div>
+
+<div className="hero-image">
+
+<img src="/public/images/shoe_image.png" alt="hero-imge" />
+
+</div>
+
+
+
+
+
+
+        </main>
+    );
+};
+
+export default Hero;
